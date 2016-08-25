49d777a0c4fe32365f3fc520643987da315a1d64
dfactory <dfactory@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-08-25 07:44:52 +0000

    Update to 1.2.36
    git-svn-id: https://plugins.svn.wordpress.org/cookie-notice@1483130 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/includes/upgrade.php
+++ trunk/includes/upgrade.php
@@ -3,30 +3,32 @@
 if ( ! defined( 'ABSPATH' ) )
 	exit;
 
-new Cookie_Notice_Update( $cookie_notice );
+new Cookie_Notice_Upgrade( $cookie_notice );
 
-class Cookie_Notice_Update {
+class Cookie_Notice_Upgrade {
+
 	private $defaults;
 
-	public function __construct( $cookie_notice )	{
+	public function __construct( $cookie_notice ) {
 		// attributes
 		$this->defaults = $cookie_notice->get_defaults();
 
 		// actions
-		add_action( 'init', array( $this, 'check_update' ) );
+		add_action( 'init', array( $this, 'check_upgrade' ) );
 	}
 
-	public function check_update() {
-		if( ! current_user_can( 'manage_options' ) )
+	public function check_upgrade() {
+		if ( ! current_user_can( 'manage_options' ) )
 			return;
 
 		// gets current database version
 		$current_db_version = get_option( 'cookie_notice_version', '1.0.0' );
 
 		// new version?
-		if( version_compare( $current_db_version, $this->defaults['version'], '<') ) {
+		if ( version_compare( $current_db_version, $this->defaults['version'], '<' ) ) {
 			// updates plugin version
 			update_option( 'cookie_notice_version', $this->defaults['version'] );
 		}
 	}
+
 }
